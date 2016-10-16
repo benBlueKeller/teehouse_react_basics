a6e11d55a35b604bdb2fb3a75e24c6b97d5c75a2
benBlueKellet <benblue22@gmail.com>
2016-10-15 18:49:31 -0700

    modified application to player AppData/Local/Temp/ReactBasics

--- app.jsx
+++ app.jsx
@@ -234,31 +234,70 @@ Player.propTypes = {
   onRemove: React.PropTypes.func.isRequired
 };
 
-function Application(props) {
-  return (
-    <div className="scoreboard">
-      <Header title={props.title} />
+var Application = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string,
+    initialPlayers: React.PropTypes.arrayOf(React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      score: React.PropTypes.number.isRequired,
+      id: React.PropTypes.number.isRequired,
+    })).isRequired,
+  },
+  
+  getDefaultProps: function() {
+    return {
+      title: "Scoreboard",
+    }
+  },
+  
+  getInitialState: function() {
+    return {
+      players: this.props.initialPlayers,
+    };
+  },
+  
+  onScoreChange: function(index, delta) {
+    this.state.players[index].score += delta;
+    this.setState(this.state);
+  },
     
-      <div className="players">
-        {props.players.map(function(player) {
-          return <Player name={player.name} score={player.score} key={player.id} />
-        })}
+  onPlayerAdd: function(name) {
+    this.state.players.push({
+      name: name,
+      score: 0,
+      id: nextId,
+    });
+    this.setState(this.state);
+    nextId += 1;
+  },
+    
+  onRemovePlayer: function(index) {
+    this.state.players.splice(index, 1);
+    this.setState(this.state);
+  },
+  
+  render: function() {
+    return (
+      <div className="scoreboard">
+        <Header title={this.props.title} players={this.state.players} />
+      
+        <div className="players">
+          {this.state.players.map(function(player, index) {
+            return (
+              <Player 
+                onScoreChange={function(delta) {this.onScoreChange(index ,delta)}.bind(this)}
+                onRemove={function() {this.onRemovePlayer(index)}.bind(this)}
+                name={player.name} 
+                score={player.score} 
+                key={player.id} />
+            );
+          }.bind(this))}
+        </div>
+        <AddPlayerForm onAdd={this.onPlayerAdd} />
       </div>
-    </div>
-  );
-}
+    );
+  }
+});
 
-Application.propTypes = {
-  title: React.PropTypes.string,
-  players: React.PropTypes.arrayOf(React.PropTypes.shape({
-    name: React.PropTypes.string.isRequired,
-    score: React.PropTypes.number.isRequired,
-    id: React.PropTypes.number.isRequired,
-  })).isRequired,
-};
-
-Application.defaultProps = {
-  title: 'Scoreboard',
-}
 
 ReactDOM.render(<Application players={PLAYERS}/>, document.getElementById('container'));
